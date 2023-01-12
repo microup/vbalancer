a2ae76db4478725b0321fefc5d930948fd492c7d
microup <ofp1979@yandex.ru>
2023-01-12 17:42:07 +0300

    replaced call peer to mock call

--- internal/proxy/peers/proxy_test.go
+++ internal/proxy/peers/proxy_test.go
@@ -1,107 +1,100 @@
 package peers_test
 
 import (
-	"sync"
+	"github.com/golang/mock/gomock"
 	"testing"
+	"vbalancer/mocks"
 
 	"vbalancer/internal/proxy/peer"
 	"vbalancer/internal/proxy/peers"
 )
 
-//nolint:funlen
+var statePeer = []struct {
+	nameTest          string
+	isAlive           []bool
+	currentPeerIndex  uint64
+	wantNextPeerIndex uint64
+}{
+	{
+		nameTest:          "test_9",
+		isAlive:           []bool{},
+		currentPeerIndex:  0,
+		wantNextPeerIndex: 0,
+	},
+	{
+		nameTest:          "test_8",
+		isAlive:           []bool{false, true, false},
+		currentPeerIndex:  1,
+		wantNextPeerIndex: 1,
+	},
+	{
+		nameTest:          "test_7",
+		isAlive:           []bool{true, false, false},
+		currentPeerIndex:  0,
+		wantNextPeerIndex: 0,
+	},
+	{
+		nameTest:          "test_6",
+		isAlive:           []bool{false, false, false},
+		currentPeerIndex:  0,
+		wantNextPeerIndex: 1,
+	},
+	{
+		nameTest:          "test_5",
+		isAlive:           []bool{false, false, true},
+		currentPeerIndex:  0,
+		wantNextPeerIndex: 2,
+	},
+	{
+		nameTest:          "test_4",
+		isAlive:           []bool{true, false, true},
+		currentPeerIndex:  0,
+		wantNextPeerIndex: 2,
+	},
+	{
+		nameTest:          "test_3",
+		isAlive:           []bool{true, true, true},
+		currentPeerIndex:  0,
+		wantNextPeerIndex: 1,
+	},
+	{
+		nameTest:          "test_2",
+		isAlive:           []bool{true, true, true},
+		currentPeerIndex:  2,
+		wantNextPeerIndex: 0,
+	},
+	{
+		nameTest:          "test_1",
+		isAlive:           []bool{true, true, true},
+		currentPeerIndex:  3,
+		wantNextPeerIndex: 0,
+	},
+}
+
 func Test_API_Get_Next_Peer(t *testing.T) {
 	t.Parallel()
 
-	cases := []struct {
-		nameTest          string
-		isAlive           []bool
-		currentPeerIndex  uint64
-		wantNextPeerIndex uint64
-	}{
-		{
-			nameTest: "test_9",
-			isAlive:  []bool{},
-			//testListPeers:             []peer.Peer{},
-			currentPeerIndex:  0,
-			wantNextPeerIndex: 0,
-		},
-		{
-			nameTest:          "test_8",
-			isAlive:           []bool{false, true, false},
-			currentPeerIndex:  1,
-			wantNextPeerIndex: 1,
-		},
-		{
-			nameTest:          "test_7",
-			isAlive:           []bool{true, false, false},
-			currentPeerIndex:  0,
-			wantNextPeerIndex: 0,
-		},
-		{
-			nameTest:          "test_6",
-			isAlive:           []bool{false, false, false},
-			currentPeerIndex:  0,
-			wantNextPeerIndex: 1,
-		},
-		{
-			nameTest:          "test_5",
-			isAlive:           []bool{false, false, true},
-			currentPeerIndex:  0,
-			wantNextPeerIndex: 2,
-		},
-		{
-			nameTest:          "test_4",
-			isAlive:           []bool{true, false, true},
-			currentPeerIndex:  0,
-			wantNextPeerIndex: 2,
-		},
-		{
-			nameTest:          "test_3",
-			isAlive:           []bool{true, true, true},
-			currentPeerIndex:  0,
-			wantNextPeerIndex: 1,
-		},
-		{
-			nameTest:          "test_2",
-			isAlive:           []bool{true, true, true},
-			currentPeerIndex:  2,
-			wantNextPeerIndex: 0,
-		},
-		{
-			nameTest:          "test_1",
-			isAlive:           []bool{true, true, true},
-			currentPeerIndex:  3,
-			wantNextPeerIndex: 0,
-		},
-	}
-
 	testListPeers := peers.New(nil)
+	ctrl := gomock.NewController(t)
 
-	//nolint:varnamelen
-	for _, c := range cases {
+	for _, c := range statePeer {
+
 		var listPeers []peer.IPeer
 
 		for _, valIsAlive := range c.isAlive {
-			pPeer := &peer.Peer{
-				Name:  "",
-				Proto: "",
-				URI:   "",
-				Mu:    &sync.RWMutex{},
-			}
-			pPeer.Mu = &sync.RWMutex{}
-			pPeer.SetAlive(valIsAlive)
-			listPeers = append(listPeers, pPeer)
+			mockPeer := mocks.NewMockIPeer(ctrl)
+			mockPeer.EXPECT().IsAlive().Return(valIsAlive).AnyTimes()
+			listPeers = append(listPeers, mockPeer)
 		}
 
 		testListPeers.List = listPeers
-		//nolint:exportloopref
 		testListPeers.CurrentPeerIndex = &c.currentPeerIndex
 
 		_, _ = testListPeers.GetNextPeer()
 
 		if *testListPeers.CurrentPeerIndex != c.wantNextPeerIndex {
-			t.Errorf("Test: %s | Result failed. got %d, want: %d",
-				c.nameTest, *testListPeers.CurrentPeerIndex, c.wantNextPeerIndex)
+			t.Errorf("Test: %s | Result failed. got %d, want: %d", c.nameTest,
+				*testListPeers.CurrentPeerIndex, c.wantNextPeerIndex)
 		}
 	}
 }
