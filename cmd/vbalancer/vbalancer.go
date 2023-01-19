0ab5b78dbff7aba7f0ad8ebac1364b7ff9d78574
microup <ofp1979@yandex.ru>
2023-01-19 18:42:22 +0300

    added nil param

--- cmd/vbalancer/vbalancer.go
+++ cmd/vbalancer/vbalancer.go
@@ -1,9 +1,9 @@
 package main
 
-import "vbalancer/internal/app"
+import (
+	"vbalancer/internal/app"
+)
 
 func main() {
-	var isAppStart = make(chan bool)
-	defer close(isAppStart)
-	app.Run(isAppStart)
+	app.Run(nil)
 }
