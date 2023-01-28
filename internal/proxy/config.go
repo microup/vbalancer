7a52f3d3cfa4ede82084a65071e778ab86c43239
microup <ofp1979@yandex.ru>
2023-01-28 21:14:11 +0300

    renamed readLineTimeMs to deadLineTimeSeconds

--- internal/proxy/config.go
+++ internal/proxy/config.go
@@ -1,8 +1,8 @@
 package proxy
 
 type Config struct {
-	DefaultPort      string `yaml:"defaultPort"`
-	DeadLineTimeMS   uint   `yaml:"deadLineTimeMs"`
-	ShutdownTimeout  uint   `yaml:"shutdownTimeout"`
-	SizeCopyBufferIO uint   `yaml:"sizeCopyBufferIo"`
+	DefaultPort         string `yaml:"defaultPort"`
+	DeadLineTimeSeconds uint   `yaml:"deadLineTimeSeconds"`
+	ShutdownTimeout     uint   `yaml:"shutdownTimeout"`
+	SizeCopyBufferIO    uint   `yaml:"sizeCopyBufferIo"`
 }
