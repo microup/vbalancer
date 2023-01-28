5c257a541ab94eda9b72b7c5d7c75db2a6865277
microup <ofp1979@yandex.ru>
2023-01-28 16:18:22 +0300

    added new var to set size copy buffer IO across client and peer.

--- internal/proxy/config.go
+++ internal/proxy/config.go
@@ -1,7 +1,8 @@
 package proxy
 
 type Config struct {
-	DefaultPort     string `yaml:"defaultPort"`
-	TimeDeadLineMS  uint   `yaml:"timeDeadLineMs"`
-	ShutdownTimeout uint   `yaml:"shutdownTimeout"`
+	DefaultPort      string `yaml:"defaultPort"`
+	DeadLineTimeMS   uint   `yaml:"deadLineTimeMs"`
+	ShutdownTimeout  uint   `yaml:"shutdownTimeout"`
+	SizeCopyBufferIO uint   `yaml:"sizeCopyBufferIO"`
 }
