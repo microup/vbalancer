7b8ece74e1b6e849dba1e7969288829d783b39b3
microup <ofp1979@yandex.ru>
2023-01-28 21:27:26 +0300

    code decomposed for proxy server

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -2,11 +2,10 @@ package proxy
 
 import (
 	"context"
-	"time"
-
 	"fmt"
 	"io"
 	"net"
+	"time"
 	"vbalancer/internal/proxy/peer"
 	"vbalancer/internal/proxy/peers"
 	"vbalancer/internal/proxy/response"
@@ -16,20 +15,19 @@ import (
 
 const (
 	maxCopyChan = 2
-	chunkSize   = 32 * 1024
 )
 
 type Proxy struct {
-	logger vlog.ILog
+	Logger vlog.ILog
 	Peers  *peers.Peers
-	cfg    *Config
+	Cfg    *Config
 }
 
 func New(cfg *Config, listPeer []peer.IPeer, logger vlog.ILog) *Proxy {
 	proxy := &Proxy{
-		logger: logger,
+		Logger: logger,
 		Peers:  peers.New(listPeer),
-		cfg:    cfg,
+		Cfg:    cfg,
 	}
 
 	return proxy
@@ -41,35 +39,44 @@ func (p *Proxy) Start(ctx context.Context, proxyPort string, checkTimeAlive *pee
 		return fmt.Errorf("%w", err)
 	}
 
+	defer func(proxySrv net.Listener) {
+		err = proxySrv.Close()
+		if err != nil {
+			p.Logger.Add(vlog.Debug, types.ErrProxy, fmt.Sprintf("proxy close failed: %v\n", err))
+		}
+	}(proxySrv)
+
 	for _, pPeer := range p.Peers.List {
 		pPeer.SetCheckTimeAlive(checkTimeAlive)
-		pPeer.SetLogger(p.logger)
+		pPeer.SetLogger(p.Logger)
 
 		go pPeer.CheckIsAlive(ctx)
 	}
 
-	return p.checkNewConnection(proxySrv)
+	p.checkNewConnection(proxySrv)
+
+	return nil
 }
 
-func (p *Proxy) checkNewConnection(proxySrv net.Listener) error {
+func (p *Proxy) checkNewConnection(proxySrv net.Listener) {
 	for {
 		conn, err := proxySrv.Accept()
 		if err != nil {
-			p.logger.Add(vlog.Debug, types.ErrProxy, vlog.RemoteAddr(conn.RemoteAddr().String()),
+			p.Logger.Add(vlog.Debug, types.ErrProxy, vlog.RemoteAddr(conn.RemoteAddr().String()),
 				fmt.Sprintf("Accept failed, %v\n", err))
 
 			continue
 		}
 
-		err = conn.SetDeadline(time.Now().Add(time.Duration(p.cfg.TimeDeadLineMS) * time.Millisecond))
+		err = conn.SetDeadline(time.Now().Add(time.Duration(p.Cfg.DeadLineTimeMS) * time.Millisecond))
 		if err != nil {
-			p.logger.Add(vlog.Debug, types.ErrProxy, vlog.RemoteAddr(conn.RemoteAddr().String()),
-				fmt.Sprintf("Accept failed: %v\n", err))
+			p.Logger.Add(vlog.Debug, types.ErrProxy, vlog.RemoteAddr(conn.RemoteAddr().String()),
+				fmt.Sprintf("failed to set deadline: %v", err))
 
-			return fmt.Errorf("failed to set deadline: %w", err)
+			continue
 		}
 
-		go p.copyConn(conn)
+		go p.handleConnection(conn)
 	}
 }
 
@@ -77,69 +84,61 @@ func (p *Proxy) Shutdown() error {
 	return nil
 }
 
-func (p *Proxy) copyConn(client net.Conn) {
+func (p *Proxy) handleConnection(client net.Conn) {
 	defer func(client net.Conn) {
 		_ = client.Close()
 	}(client)
 
-	p.logger.Add(vlog.Debug, types.ResultOK, vlog.RemoteAddr(client.RemoteAddr().String()))
+	p.Logger.Add(vlog.Debug, types.ResultOK, vlog.RemoteAddr(client.RemoteAddr().String()))
 
 	pPeer, resultCode := p.Peers.GetNextPeer()
 
 	if resultCode != types.ResultOK || pPeer == nil {
 		if pPeer != nil {
-			p.logger.Add(vlog.Debug, resultCode, vlog.RemoteAddr(client.RemoteAddr().String()),
+			p.Logger.Add(vlog.Debug, resultCode, vlog.RemoteAddr(client.RemoteAddr().String()),
 				vlog.ProxyHost(pPeer.GetURI()), resultCode.ToStr())
 		} else {
-			p.logger.Add(vlog.Debug, resultCode, vlog.RemoteAddr(client.RemoteAddr().String()), resultCode.ToStr())
+			p.Logger.Add(vlog.Debug, resultCode, vlog.RemoteAddr(client.RemoteAddr().String()), resultCode.ToStr())
 		}
 
-		responseLogger := response.New(p.logger)
+		responseLogger := response.New(p.Logger)
 		_ = responseLogger.SentResponse(client, resultCode)
 
 		return
 	}
 
-	dst, err := net.Dial("tcp", pPeer.GetURI())
+	dst, err := net.DialTimeout("tcp", pPeer.GetURI(), time.Duration(p.Cfg.DeadLineTimeMS)*time.Millisecond)
 	if err != nil {
-		p.logger.Add(vlog.Debug, types.ErrProxy, vlog.RemoteAddr(client.RemoteAddr().String()),
-			fmt.Sprintf("Accept failed, %v\n", err))
+		p.Logger.Add(vlog.Debug, types.ErrProxy, vlog.RemoteAddr(client.RemoteAddr().String()),
+			fmt.Sprintf("failed connecting to target:, %v\n", err))
 
-		responseLogger := response.New(p.logger)
+		responseLogger := response.New(p.Logger)
 		_ = responseLogger.SentResponse(client, types.ErrProxy)
 
 		return
 	}
 
-	p.logger.Add(vlog.Debug, types.ResultOK, vlog.RemoteAddr(client.RemoteAddr().String()),
+	p.Logger.Add(vlog.Debug, types.ResultOK, vlog.RemoteAddr(client.RemoteAddr().String()),
 		vlog.ProxyHost(pPeer.GetURI()))
 
-	p.proxyDataCopy(client, dst)
-
+	p.ProxyDataCopy(client, dst)
 }
 
-func (p *Proxy) proxyDataCopy(client net.Conn, dst net.Conn) {
+func (p *Proxy) ProxyDataCopy(client net.Conn, dst io.ReadWriteCloser) {
 	done := make(chan struct{}, maxCopyChan)
 	defer close(done)
 
+	p.CopyDataPeerToClient(dst, client, done)
 	p.copyDataClientToPeer(client, dst, done)
-	p.copyDataPeerToClient(dst, client, done)
 
 	<-done
 	<-done
 }
 
-func (p *Proxy) copyDataClientToPeer(client net.Conn, dst net.Conn, done chan struct{}) {
-	writeBuffer := make([]byte, chunkSize)
-
+func (p *Proxy) copyDataClientToPeer(client net.Conn, dst io.ReadCloser, done chan struct{}) {
 	go func() {
-		var errCopy error
-		_, errCopy = io.CopyBuffer(client, dst, writeBuffer)
-
-		if errCopy != nil {
-			p.logger.Add(vlog.Debug, types.ErrCopyDataClientToPeer, vlog.RemoteAddr(client.RemoteAddr().String()),
-				fmt.Sprintf(types.ErrorCopyDataClientToPeer, errCopy))
-		}
+		writeBuffer := make([]byte, p.Cfg.SizeCopyBufferIO)
+		_, _ = io.CopyBuffer(client, dst, writeBuffer)
 
 		_ = dst.Close()
 		_ = client.Close()
@@ -147,17 +146,10 @@ func (p *Proxy) copyDataClientToPeer(client net.Conn, dst net.Conn, done chan st
 	}()
 }
 
-func (p *Proxy) copyDataPeerToClient(dst net.Conn, client net.Conn, done chan struct{}) {
-	readBuffer := make([]byte, chunkSize)
-
+func (p *Proxy) CopyDataPeerToClient(dst io.WriteCloser, client net.Conn, done chan struct{}) {
 	go func() {
-		var errCopy error
-		_, errCopy = io.CopyBuffer(dst, client, readBuffer)
-
-		if errCopy != nil {
-			p.logger.Add(vlog.Debug, types.ErrCopyDataPeerToClient, vlog.RemoteAddr(client.RemoteAddr().String()),
-				fmt.Sprintf(types.ErrorCopyDataPeerToClient, errCopy))
-		}
+		readBuffer := make([]byte, p.Cfg.SizeCopyBufferIO)
+		_, _ = io.CopyBuffer(dst, client, readBuffer)
 
 		_ = dst.Close()
 		_ = client.Close()
