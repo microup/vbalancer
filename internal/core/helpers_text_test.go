c711193a58a6e94ade33a2ddd3285300a6b5a32a
microup <ofp1979@yandex.ru>
2023-01-29 22:45:19 +0300

    added test func TestFmtStringWithDelimiter()

--- internal/core/helpers_text_test.go
+++ internal/core/helpers_text_test.go
@@ -5,7 +5,6 @@ import (
 	"vbalancer/internal/core"
 )
 
-
 func TestTrimLastChar(t *testing.T) {
 	t.Parallel()
 
@@ -26,6 +25,33 @@ func TestTrimLastChar(t *testing.T) {
 		got := core.TrimLastChar(tc.input)
 		if got != tc.want {
 			t.Errorf("TrimLastChar(%q) = %q, want %q", tc.input, got, tc.want)
+		}
+	}
+}
+
+func TestFmtStringWithDelimiter(t *testing.T) {
+	t.Parallel()
+
+	delimiter := ","
+	tests := []struct {
+		values  []interface{}
+		result  string
+		isError bool
+	}{
+		{[]interface{}{"a", "b", "c"}, "a,b,c", false},
+		{[]interface{}{1, 2, 3}, "1,2,3", false},
+		{[]interface{}{"a", 1, []int{1, 2, 3}}, "a,1,1,2,3", false},
+		{[]interface{}{"a", nil, "c"}, "a,c", false},
+		{[]interface{}{"a", "b", "c\nd"}, "a,b,c d", false},
+		{[]interface{}{"a", "b", "c", []int{1, 2, 3}}, "a,b,c,1,2,3", false},
+		{[]interface{}{1, 2, []string{"a", "b", "c"}}, "1,2,a,b,c", false},
+		{[]interface{}{1, 2, "c", []int{}}, "1,2,c", false},
+	}
+
+	for _, test := range tests {
+		result := core.FmtStringWithDelimiter(delimiter, test.values...)
+		if result != test.result {
+			t.Errorf("Expected %s, but got %s", test.result, result)
 		}
 	}
 }
