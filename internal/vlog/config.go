1c819aad282f3c734edb997a9f514df98c9c9fbc
microup <ofp1979@yandex.ru>
2023-01-29 16:03:11 +0300

    removed not used vars LogWriteSec and KindType

--- internal/vlog/config.go
+++ internal/vlog/config.go
@@ -4,6 +4,4 @@ type Config struct {
 	DirLog         string `yaml:"dirLog"`
 	FileSize       uint64 `yaml:"fileSize"`
 	APIShowRecords uint64 `yaml:"apiShowRecords"`
-	LogWriteSec    uint64 `yaml:"logWriteSec"`
-	KindType       string `yaml:"kindType"`
 }
