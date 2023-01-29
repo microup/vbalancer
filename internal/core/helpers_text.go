7fcb74cb23aaab76057abca1489ec3de19c3375c
microup <ofp1979@yandex.ru>
2023-01-29 22:10:22 +0300

    decomposition func FmtStringWithDelimiter()

--- internal/core/helpers_text.go
+++ internal/core/helpers_text.go
@@ -13,48 +13,22 @@ func TrimLastChar(value string) string {
 		if size == len(value) {
 			return ""
 		}
-		
+
 		return value[:len(value)-size]
 	}
 
 	return ""
 }
 
-//nolint:cyclop
 func FmtStringWithDelimiter(delimiter string, values ...interface{}) *string {
 	resultStr := ""
 
 	for _, value := range values {
-		valueStr := ""
-
 		if value == nil {
 			continue
 		}
 
-		//nolint:exhaustive
-		switch reflect.TypeOf(value).Kind() {
-		case reflect.String:
-			valueStr = fmt.Sprintf("%s", value)
-		case reflect.Int:
-			valueInt, ok := value.(int)
-			if !ok {
-				continue
-			}
-
-			valueStr = fmt.Sprintf("%d", valueInt)
-		case reflect.Slice:
-			s := reflect.ValueOf(value)
-			for i := 0; i < s.Len(); i++ {
-				val := s.Index(i)
-				if valueStr == "" {
-					valueStr = fmt.Sprintf("%s", val)
-				} else {
-					valueStr = valueStr + delimiter + fmt.Sprintf("%s", val)
-				}
-			}
-		default:
-			valueStr = fmt.Sprintf("%v", value)
-		}
+		valueStr := getValueStr(delimiter, value)
 
 		if resultStr == "" {
 			resultStr = valueStr
@@ -67,4 +41,35 @@ func FmtStringWithDelimiter(delimiter string, values ...interface{}) *string {
 	resultStr = reg.ReplaceAllString(resultStr, " ")
 
 	return &resultStr
+}
+
+func getValueStr(delimiter string, value interface{}) string {
+	valueStr := ""
+
+	//nolint:exhaustive
+	switch reflect.TypeOf(value).Kind() {
+	case reflect.String:
+		valueStr = fmt.Sprintf("%s", value)
+	case reflect.Int:
+		valueInt, ok := value.(int)
+		if !ok {
+			return ""
+		}
+
+		valueStr = fmt.Sprintf("%d", valueInt)
+	case reflect.Slice:
+		s := reflect.ValueOf(value)
+		for i := 0; i < s.Len(); i++ {
+			val := s.Index(i)
+			if valueStr == "" {
+				valueStr = fmt.Sprintf("%s", val)
+			} else {
+				valueStr = valueStr + delimiter + fmt.Sprintf("%s", val)
+			}
+		}
+	default:
+		valueStr = fmt.Sprintf("%v", value)
+	}
+
+	return valueStr
 }
