56cbfb9863638557b0458b1ceddff1e0e2701576
microup <ofp1979@yandex.ru>
2023-01-31 21:06:26 +0300

    fixed tests

--- internal/proxy/proxy_test.go
+++ internal/proxy/proxy_test.go
@@ -3,11 +3,10 @@ package proxy_test
 import (
 	"bytes"
 	"context"
-	"io"
 	"net"
 	"testing"
-	"time"
 	"vbalancer/internal/proxy"
+	"vbalancer/mocks"
 )
 
 func TestCheckNewConnection(t *testing.T) {
@@ -21,10 +20,8 @@ func TestCheckNewConnection(t *testing.T) {
 
 	//nolint:exhaustivestruct,exhaustruct
 	testProxy := &proxy.Proxy{
-		Cfg: &proxy.Config{DeadLineTimeMS: 100},
-		Logger: &MockLogger{
-			t: t,
-		},
+		Cfg:    &proxy.Config{DeadLineTimeMS: 100},
+		Logger: &mocks.MockLogger{},
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -43,7 +40,6 @@ func TestCheckNewConnection(t *testing.T) {
 func TestProxyDataCopy(t *testing.T) {
 	t.Parallel()
 
-	// Create a test case with sample input and expected output data
 	testCases := []struct {
 		clientData   []byte
 		peerData     []byte
@@ -54,88 +50,19 @@ func TestProxyDataCopy(t *testing.T) {
 			peerData:     []byte{},
 			expectedData: []byte("Hello World"),
 		},
-		{
-			clientData:   []byte("123456789"),
-			peerData:     []byte{},
-			expectedData: []byte("123456789"),
-		},
 	}
 
 	for _, cases := range testCases {
-		client := &FakeConn{data: cases.clientData} //nolint:exhaustivestruct,exhaustruct
-		peer := &FakeConn{data: []byte{}} //nolint:exhaustivestruct,exhaustruct
+		client := &mocks.MockConn{Data: cases.clientData, IsClient: true, Pos: 0} 
+		peer := &mocks.MockConn{Data: cases.peerData, IsClient: false, Pos: 0}           
 
-		proxyTest := &proxy.Proxy{Cfg: &proxy.Config{SizeCopyBufferIO: 1024}} //nolint:exhaustivestruct,exhaustruct
+		//nolint:exhaustivestruct,exhaustruct
+		proxyTest := &proxy.Proxy{Cfg: &proxy.Config{SizeCopyBufferIO: 64}} 
 
 		proxyTest.ProxyDataCopy(client, peer)
 
-		done := make(chan struct{}, 2)
-		proxyTest.CopyDataClientToPeer(peer, client, done)
-		done <- struct{}{}
-
-		if !bytes.Equal(peer.data, cases.expectedData) {
-			t.Errorf("Expected %q, but got %q", cases.expectedData, client.data)
+		if !bytes.Equal(peer.Data, cases.expectedData) {
+			t.Errorf("Expected %q, but got %q", cases.expectedData, client.Data)
 		}
 	}
-}
-
-type MockLogger struct {
-	t *testing.T
-}
-
-func (m *MockLogger) Add(values ...interface{}) {
-
-}
-
-func (m *MockLogger) Close() error {
-	return nil
-}
-
-
-// fakeConn is a fake implementation of the net.Conn interface
-type FakeConn struct {
-	data []byte
-	pos  int
-}
-
-func (c *FakeConn) Read(b []byte) (int, error) {
-	if c.pos >= len(c.data) {
-		return 0, io.EOF
-	}
-
-	n := copy(b, c.data[c.pos:])
-	c.pos += n
-
-	return n, nil
-}
-
-func (c *FakeConn) Write(b []byte) (int, error) {
-	c.data = append(c.data, b...)
-	c.pos += len(b)
-
-	return len(b), nil
-}
-
-func (c *FakeConn) Close() error {
-	return nil
-}
-
-func (c *FakeConn) LocalAddr() net.Addr {
-	return nil
-}
-
-func (c *FakeConn) RemoteAddr() net.Addr {
-	return nil
-}
-
-func (c *FakeConn) SetDeadline(t time.Time) error {
-	return nil
-}
-
-func (c *FakeConn) SetReadDeadline(t time.Time) error {
-	return nil
-}
-
-func (c *FakeConn) SetWriteDeadline(t time.Time) error {
-	return nil
 }
