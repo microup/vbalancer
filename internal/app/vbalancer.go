06542cdd61864f6885066b30119da8bd68a63317
microup <ofp1979@yandex.ru>
2023-08-07 08:39:39 +0300

    fix: improve core logic

--- internal/app/vbalancer.go
+++ internal/app/vbalancer.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -16,79 +17,69 @@ import (
 	"vbalancer/internal/vlog"
 )
 
+var ErrRecoveredPanic = errors.New("recovered from panic")
+
 // Run this is the function of an application that starts a proxy server.
 func Run() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	
+
+	cfg := config.New()
+
+	err := cfg.Init()
+	if err != nil {
+		log.Panicf("failed to initialize configuration: %s", err.Error())
+	}
+
+	logger, err := vlog.New(cfg.Logger)
+	if err != nil {
+		log.Panicf("failed to create logger: %s", err)
+	}
+
 	defer func() {
 		if err := recover(); err != nil {
-			fmt.Printf("catch err: %v", err) //nolint:forbidigo
+			logger.Add(vlog.Fatal, types.ErrGotPanic, fmt.Errorf("%w: %v", ErrRecoveredPanic, err))
 		}
 	}()
-
-	configuration := initializeConfig()
-
-	logger, err := vlog.New(configuration.Logger)
-	if err != nil {
-		log.Panicf("failed created logger: %v", err)
-	}
 
 	defer func(logger vlog.ILog) {
 		err = logger.Close()
 		if err != nil {
-			log.Fatalf("failed closed logger: %v", err)
+			log.Fatalf("failed close logger: %v", err)
 		}
 	}(logger)
 
-	ctx, cancel := context.WithTimeout(context.Background(), configuration.Proxy.ShutdownTimeout)
+	ctx, cancel := context.WithTimeout(context.Background(), cfg.Proxy.ShutdownTimeout)
 	defer cancel()
-	
-	peerList := createPeerListForBalancer(configuration)
 
-	proxyBalancer := proxy.New(configuration.Proxy, configuration.Rules, peerList, logger)
+	peerList := newPeerList(cfg)
 
-	logger.Add(vlog.Info, types.ResultOK, fmt.Sprintf("start server addr on %s", configuration.ProxyPort))
-
-	go func() {
-		if err = proxyBalancer.ListenAndServe(ctx, configuration.ProxyPort); err != nil {
-			logger.Add(vlog.Fatal, types.ErrProxy, fmt.Errorf("can't start proxy %w", err))
-		}
-	}()
-
-	logger.Add(vlog.Info, types.ResultOK, "the balancer is running")
+	proxyBalancer := proxy.New(cfg.Proxy, cfg.Rules, peerList, logger)
 
 	stopSignal := make(chan os.Signal, 1)
 	signal.Notify(stopSignal, os.Interrupt, syscall.SIGTERM)
+
+	listenProxyChan := make(chan error)
+
+	go func() {
+		logger.Add(vlog.Info, types.ResultOK, fmt.Sprintf("start server addr on %s", cfg.ProxyPort))
+		listenProxyChan <- proxyBalancer.ListenAndServe(ctx, cfg.ProxyPort)
+	}()
+
+	listenErr := <-listenProxyChan
+	if listenErr != nil {
+		logger.Add(vlog.Fatal, types.ErrProxy, fmt.Errorf("the proxy was return err: %w", err))
+	}
+
 	<-stopSignal
 }
 
-// initializeConfig is the function that initializes the configuration of the application.
-func initializeConfig() *config.Config {
-	configFile := os.Getenv("ConfigFile")
-
-	cfg := config.New()
-
-	if configFile == "" {
-		configFile = config.DefaultConfigFile
-	}
-
-	if err := cfg.Load(configFile); err != nil {
-		log.Fatalf("%v", err)
-	}
-
-	if resultCode := cfg.InitProxyPort(); resultCode != types.ResultOK {
-		log.Fatalf("can't init proxy: %s", resultCode.ToStr())
-	}
-
-	return cfg
-}
-
-// createPeerListForBalancer is the function that creates a list of peers for the balancer.
-func createPeerListForBalancer(cfg *config.Config) []peer.IPeer {
+// newPeerList is the function that creates a list of peers for the balancer.
+func newPeerList(cfg *config.Config) []peer.IPeer {
 	listPeer := make([]peer.IPeer, len(cfg.Peers))
 
-	for index, valPeer := range cfg.Peers {
-		listPeer[index] = valPeer
+	for index, cfgPeer := range cfg.Peers {
+		peerCopy := cfgPeer 
+		listPeer[index] = &peerCopy
 	}
 
 	return listPeer
