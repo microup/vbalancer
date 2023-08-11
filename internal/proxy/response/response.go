69a75489c40842f3f052492bef3a01411772810b
microup <ofp1979@yandex.ru>
2023-08-11 13:29:31 +0300

    feat: remove logger

--- internal/proxy/response/response.go
+++ internal/proxy/response/response.go
@@ -4,23 +4,21 @@ import (
 	"encoding/json"
 	"fmt"
 	"net"
+	
 	"vbalancer/internal/types"
-	"vbalancer/internal/vlog"
 )
 
 // Response is a struct that contains the response to the client.
 type Response struct {
 	StatusCode  types.ResultCode `json:"statusCode"`
 	Description string           `json:"description"`
-	logger      vlog.ILog
 }
 
 // New create a new response object.
-func New(logger vlog.ILog) *Response {
+func New() *Response {
 	return &Response{
 		StatusCode:  types.ResultUnknown,
 		Description: "",
-		logger:      logger,
 	}
 }
 
@@ -30,13 +28,7 @@ func (r *Response) SentResponseToClient(client net.Conn, err error) error {
 	r.Description = err.Error()
 
 	responseJSON, err := json.Marshal(r)
-	
 	if err != nil {
-		r.logger.Add(vlog.Debug,
-			types.ErrCantMarshalJSON,
-			vlog.RemoteAddr(client.RemoteAddr().String()),
-			types.ErrCantMarshalJSON.ToStr())
-
 		return fmt.Errorf("%w", err)
 	}
 
@@ -48,12 +40,8 @@ func (r *Response) SentResponseToClient(client net.Conn, err error) error {
 			"Content-Length: %d\r\n\r\n"+
 			"%s", responseLen, responseJSON)
 
-		
 	_, err = client.Write([]byte(responseBody))
 	if err != nil {
-		r.logger.Add(vlog.Debug, types.ErrSendResponseToClient, vlog.RemoteAddr(client.RemoteAddr().String()),
-			types.ErrSendResponseToClient.ToStr())
-
 		return fmt.Errorf("%w", err )
 	}
 
