36d3b4707ecddd635a53bc3e05f310fe1b6c9d47
microup <ofp1979@yandex.ru>
2023-08-11 13:28:14 +0300

    style: small changes

--- internal/proxy/proxy_test.go
+++ internal/proxy/proxy_test.go
@@ -96,7 +96,7 @@ func TestGetProxyPort(t *testing.T) {
 			name:       "empty env var, got DefaultPort",
 			envVar:     "",
 			expected:   types.ResultOK,
-			checkValue: fmt.Sprintf(":%d", proxy.DefaultPort),
+			checkValue: fmt.Sprintf(":%d", types.DefaultProxyPort),
 		},
 		{
 			name:       "valid proxy port from env var",
